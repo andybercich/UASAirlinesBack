8500cc1f8204092b18f5c7164728d773f3a805b8
ANDRÉS BERCICH <andybercich6@gmail.com>
2025-01-31 15:06:23 -0300

    Mayoria de endpoints probados, hay que avanzar con los demás para probar los que faltan

--- src/main/java/org/example/Entities/Pais.java
+++ src/main/java/org/example/Entities/Pais.java
@@ -1,5 +1,7 @@
 package org.example.Entities;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,12 @@ import java.util.Set;
 @Data
 public class Pais extends Base {
 
+    @NotNull(message = "El país debe tener un nombre")
+    @NotBlank(message = "El país debe tener un nombre")
     private String nombre;
 
     @Enumerated(value = EnumType.STRING)
+    @NotNull(message = "El país debe tener un continente asociado")
     private Continente continente;
 
 }
